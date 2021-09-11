90f3cda7f260664a34921cb2476a07f895410f70
Technote <technote.space@gmail.com>
2021-09-12 00:59:14 +0900

    chore: add lint check

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const Hello = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: 'Hello' });
-}
+};
+export default Hello;
