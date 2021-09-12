b6578f4ea130ca2d98cbffbc3b65fd22b8b31c95
Technote <technote.space@gmail.com>
2021-09-13 00:08:00 +0900

    chore: remove unused api

--- src/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const Hello = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: 'Hello' });
-};
-export default Hello;
