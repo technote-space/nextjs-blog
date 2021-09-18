0ed456db8d5c688f3a2034a85d35984c5416d7c7
Technote <technote.space@gmail.com>
2021-09-19 01:56:19 +0900

    chore: revert to use webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const tsconfigPaths = require('vite-tsconfig-paths').default;
+const {TsconfigPathsPlugin} = require('tsconfig-paths-webpack-plugin');
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  core: {builder: "storybook-builder-vite"},
   "stories": [
     "../src/components/Introduction.stories.mdx",
     "../src/**/*.stories.mdx",
@@ -16,22 +15,25 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-links",
   ],
-  viteFinal: async (config) => {
-    config.plugins.push(tsconfigPaths({root: '..'}));
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@emotion/core": toPath("node_modules/@emotion/react"),
-      "emotion-theming": toPath("node_modules/@emotion/react"),
-    };
-    config.define = {
-      'process.env': process.env
-    };
-    config.optimizeDeps.include.push(
-      'reflect-metadata', 'tsyringe', 'storybook-addon-performance',
-      'dayjs', '@chakra-ui/react', '@chakra-ui/theme-tools'
-    );
-    config.esbuild = {jsxInject: 'import React from \'react\';'};
-
-    return config;
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      node: {
+        fs: 'empty'
+      },
+      resolve: {
+        ...config.resolve,
+        plugins: [
+          new TsconfigPathsPlugin({
+            configFile: path.resolve(__dirname, '../tsconfig.json')
+          }),
+        ],
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        }
+      }
+    }
   },
 }
