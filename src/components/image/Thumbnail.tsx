3e68eff5e23224c2f064b1982e78fe14da048e44
Technote <technote.space@gmail.com>
2021-09-20 00:28:55 +0900

    chore: tweaks

--- src/components/image/Thumbnail.tsx
+++ src/components/image/Thumbnail.tsx
@@ -7,8 +7,8 @@ type Props = ImageProps;
 const defaultProps: Props = {
   alt: 'thumbnail',
   objectFit: 'cover',
-  htmlWidth: 150,
-  htmlHeight: 150,
+  width: 150,
+  height: 150,
   loading: 'lazy',
   flexShrink: 0,
 };
@@ -16,6 +16,8 @@ const defaultProps: Props = {
 const processProps = (props: Props) => {
   return {
     ...props,
+    htmlWidth: props.htmlWidth ?? (typeof props.width === 'number' ? props.width : Array.isArray(props.width) ? (props.width[0] as number) : undefined),
+    htmlHeight: props.htmlHeight ?? (typeof props.height === 'number' ? props.height : Array.isArray(props.height) ? (props.height[0] as number) : undefined),
     width: props.width ?? props.htmlWidth,
     height: props.height ?? props.htmlHeight,
   };
--- src/packages/infra/pages/components/Card.tsx
+++ src/packages/infra/pages/components/Card.tsx
@@ -21,8 +21,8 @@ type Props = StyleProps & {
 const Card: VFC<Props> = ({ url, thumbnail, title, excerpt, createdAt, dateFormat, ...props }) => {
   return <Link href={url}>
     <a>
-      <Flex flexDir="row" backgroundColor="white" p={3} cursor="pointer" {...props}>
-        <Thumbnail src={thumbnail} htmlWidth={300} htmlHeight={180}/>
+      <Flex flexDir="row" backgroundColor="white" p={3} cursor="pointer" alignItems="center" {...props}>
+        <Thumbnail src={thumbnail} width={[150, 200, 200, 300]} height={[100, 120, 120, 180]}/>
         <Flex flexDir="column" p={3} flexGrow={1}>
           <MainHeading mb={4}>
             {title}
