e617dddf751c588ee27a953311c48618ab4be48f
Technote <technote.space@gmail.com>
2021-09-20 12:34:39 +0900

    chore: use esbuild

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  clearMocks: true,
+  testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
+  ],
+  coveragePathIgnorePatterns: [
+    '\\$.+\\.ts',
   ],
   moduleNameMapper: {
     /* Handle CSS imports (with CSS modules)
@@ -26,7 +31,8 @@ module.exports = {
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}],
+    '^.+\\.(js|jsx|ts)$': 'esbuild-jest',
+    '^.+\\.tsx$': ['babel-jest', {presets: ['next/babel']}],
   },
   transformIgnorePatterns: [
     '^.+\\.module\\.(css|sass|scss)$',
--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
+const {ESBuildMinifyPlugin} = require('esbuild-loader');
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+function useEsbuildMinify(config, options) {
+  const {minimizer} = config.optimization;
+  const terserIndex = minimizer.findIndex(
+    minifier => minifier.constructor.name === 'TerserPlugin',
+  );
+
+  minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+}
+
+function useEsbuildLoader(config, options) {
+  const {rules} = config.module;
+  const rule = rules.find(rule => rule.test.test('.js'));
+
+  rule.use = {
+    loader: 'esbuild-loader',
+    options,
+  };
+}
+
 module.exports = withBundleAnalyzer({
   webpack5: true,
-  webpack: (config, {isServer}) => {
+  webpack: (config, {isServer, webpack}) => {
     config.resolve.fallback = {
       fs: false,
       path: false,
@@ -25,6 +45,17 @@ module.exports = withBundleAnalyzer({
         {'remove-markdown': 'var {}'},
       );
     }
+
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+    );
+    useEsbuildMinify(config);
+    useEsbuildLoader(config, {
+      loader: 'tsx',
+      target: 'es2017',
+    });
 
     return config;
   },
