aa858384767b43fdd37f99cd161d4d9502d4eaec
Technote <technote.space@gmail.com>
2021-10-01 01:10:51 +0900

    chore: tweaks

--- src/packages/infra/post/service/code.ts
+++ src/packages/infra/post/service/code.ts
@@ -1,13 +1,13 @@
 import type { ICodeService } from '$/domain/post/service/code';
+import parse from 'rehype-parse';
+import shiki from 'rehype-shiki';
+import stringify from 'rehype-stringify';
 import { singleton } from 'tsyringe';
+import { unified } from 'unified';
 
 @singleton()
 export class CodeService implements ICodeService {
   public async process(html: string): Promise<string> {
-    const { unified } = await import ('unified');
-    const parse = (await import('rehype-parse')).default;
-    const shiki = (await import('rehype-shiki')).default;
-    const stringify = (await import('rehype-stringify')).default;
     return (await unified()
       .use(parse)
       .use(shiki)
