61acd1ce21f053bf9bb871042bd6169b11328440
Technote <technote.space@gmail.com>
2021-10-01 00:44:08 +0900

    chore: remove esbuild

--- jest.config.js
+++ jest.config.js
@@ -31,8 +31,7 @@ module.exports = {
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts)$': 'esbuild-jest',
-    '^.+\\.tsx$': ['babel-jest', {presets: ['next/babel']}],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}],
   },
   transformIgnorePatterns: [
     '^.+\\.module\\.(css|sass|scss)$',
--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-const {ESBuildMinifyPlugin} = require('esbuild-loader');
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-
-function useEsbuildMinify(config, options) {
-  const {minimizer} = config.optimization;
-  const terserIndex = minimizer.findIndex(
-    minifier => minifier.constructor.name === 'TerserPlugin',
-  );
-
-  minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
-}
-
-function useEsbuildLoader(config, options) {
-  const {rules} = config.module;
-  const rule = rules.find(rule => rule.test.test('.js'));
-
-  rule.use = {
-    loader: 'esbuild-loader',
-    options,
-  };
-}
 
 module.exports = withBundleAnalyzer({
   webpack5: true,
@@ -46,11 +26,6 @@ module.exports = withBundleAnalyzer({
         React: 'react',
       }),
     );
-    useEsbuildMinify(config);
-    useEsbuildLoader(config, {
-      loader: 'tsx',
-      target: 'es2017',
-    });
 
     return config;
   },
