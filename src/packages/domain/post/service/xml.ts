fd61dc1ec80bdf397a70cfe13f20e1c23d382160
Technote <technote.space@gmail.com>
2021-10-02 19:13:15 +0900

    chore: tweaks

--- src/packages/domain/post/service/xml.ts
+++ src/packages/domain/post/service/xml.ts
@@ -1,4 +1,3 @@
 export interface IXmlService {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   parse<T>(xmlContent: string): Promise<T>;
 }
--- src/packages/infra/post/service/xml.ts
+++ src/packages/infra/post/service/xml.ts
@@ -4,7 +4,6 @@ import xml2js from 'xml2js';
 
 @singleton()
 export class XmlService implements IXmlService {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async parse<T>(xmlContent: string): Promise<T> {
     return xml2js.parseStringPromise(xmlContent, {
       trim: true,
