79a0f08053e47588400b5bd1f79837fbad64ab9a
Technote <technote.space@gmail.com>
2021-10-07 01:14:18 +0900

    chore: tweaks

--- src/packages/infra/post/repository/mdPost.ts
+++ src/packages/infra/post/repository/mdPost.ts
@@ -70,7 +70,7 @@ export class MarkdownPostRepository extends BasePostRepository implements IPostR
       return join(process.cwd(), 'posts');
     }
 
-    return join(process.cwd(), '.next', 'static', 'posts');
+    return join('/', '_next', 'static', 'posts');
   }
 
   private static toMaybePost(id: string, result: matter.GrayMatterFile<string>): MaybePost {
