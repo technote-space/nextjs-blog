91af22d01b9a40c886ce51cc944cb6bc736a0629
Technote <technote.space@gmail.com>
2021-10-09 22:21:26 +0900

    chore: update lint settings

--- src/config/registry.shared.ts
+++ src/config/registry.shared.ts
@@ -1,11 +1,11 @@
 import { container } from 'tsyringe';
-import { FooterComponent } from '$/infra/app/layout/footer';
-import { HeaderComponent } from '$/infra/app/layout/header';
-import { LayoutComponent } from '$/infra/app/layout';
+import { GoogleAdsense } from '$/infra/advertising/google';
+import { GoogleAnalytics } from '$/infra/analytics/google';
 import { HeadComponent } from '$/infra/app/head';
 import { AppService } from '$/infra/app/index';
-import { GoogleAnalytics } from '$/infra/analytics/google';
-import { GoogleAdsense } from '$/infra/advertising/google';
+import { LayoutComponent } from '$/infra/app/layout';
+import { FooterComponent } from '$/infra/app/layout/footer';
+import { HeaderComponent } from '$/infra/app/layout/header';
 import { IndexPage } from '$/infra/pages/index';
 import { PostPage } from '$/infra/pages/post';
 
