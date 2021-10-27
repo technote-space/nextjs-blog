b3486dfd92bdfc623ec3a2a17b47676aa639dd28
Technote <technote.space@gmail.com>
2021-10-27 23:49:59 +0900

    chore: for fast-average-color-node error

--- src/packages/infra/post/service/color.ts
+++ src/packages/infra/post/service/color.ts
@@ -1,15 +1,21 @@
 import type { IColorService } from '$/domain/post/service/color';
-import { singleton } from 'tsyringe';
+import { inject, singleton } from 'tsyringe';
+import { Settings } from '$/domain/app/settings';
 import DominantColor from '$/domain/post/valueObject/dominantColor';
+import { loadImage } from '@/lib/helpers/image';
 
 @singleton()
 export class ColorService implements IColorService {
-  public async getDominantColor(imageUrl?: string, siteUrl?: string, retry?: number): Promise<DominantColor | undefined> {
+  public constructor(@inject('Settings') private settings: Settings) {
+  }
+
+  public async getDominantColor(imageUrl: string | undefined, siteUrl: string, retry?: number): Promise<DominantColor | undefined> {
     if (imageUrl) {
-      const url = imageUrl.startsWith('/') ? `${siteUrl?.replace(/\/$/, '')}${imageUrl}` : imageUrl;
+      const url = imageUrl.startsWith('/') ? `${siteUrl.replace(/\/$/, '')}${imageUrl}` : imageUrl;
+      const imageBuffer = await loadImage(url, siteUrl);
       for (let i = retry ?? 3; --i >= 0;) {
         try {
-          const color = await (await import('fast-average-color-node')).getAverageColor(url, {
+          const color = await (await import('fast-average-color-node')).getAverageColor(imageBuffer, {
             defaultColor: [255, 255, 255, 255],
           });
           return DominantColor.create(color.rgba);
