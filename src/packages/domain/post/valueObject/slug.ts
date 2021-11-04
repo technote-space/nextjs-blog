d7cb5f09f456b44b2b94125401f446ced4573a8c
Technote <technote.space@gmail.com>
2021-11-05 02:07:33 +0900

    chore: add tests

--- src/packages/domain/post/valueObject/slug.ts
+++ src/packages/domain/post/valueObject/slug.ts
@@ -2,6 +2,6 @@ import StringId from '$/domain/shared/valueObject/stringId';
 
 export default class Slug extends StringId {
   public getName(): string {
-    return 'タグID';
+    return 'Slug';
   }
 }
