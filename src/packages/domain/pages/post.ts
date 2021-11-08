334e866d3ce0804f2808131caacd1b48d5cd9ddd
Technote <technote.space@gmail.com>
2021-11-09 01:21:34 +0900

    feat: add prev and next post

--- src/packages/domain/pages/post.ts
+++ src/packages/domain/pages/post.ts
@@ -1,9 +1,12 @@
+import type { PostDTO } from '$/domain/post/dto/post';
 import type { PostDetailDTO } from '$/domain/post/dto/postDetail';
 import type { IPage, ServerProps } from '$/domain/shared/page';
 import type { GetStaticPathsResult, GetStaticPropsResult } from 'next';
 
 export type Props = ServerProps & {
   post: PostDetailDTO;
+  prev: PostDTO | null;
+  next: PostDTO | null;
 };
 export type Params = {
   id: string;
--- src/packages/infra/pages/post/hooks.ts
+++ src/packages/infra/pages/post/hooks.ts
@@ -1,10 +1,11 @@
 import type { Settings } from '$/domain/app/settings';
 import type { Props } from '$/domain/pages/post';
-import { toEntity } from '$/domain/post/dto/postDetail';
+import { toEntity as toPostEntity } from '$/domain/post/dto/post';
+import { toEntity as toPostDetailEntity } from '$/domain/post/dto/postDetail';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const useHooks = ({ headerPages, footerPages, post }: Props, settings: Settings) => {
-  const entity = toEntity(post);
+export const useHooks = ({ headerPages, footerPages, post, prev, next }: Props, settings: Settings) => {
+  const entity = toPostDetailEntity(post);
 
   return {
     layoutProps: {
@@ -19,6 +20,8 @@ export const useHooks = ({ headerPages, footerPages, post }: Props, settings: Se
     },
     viewProps: {
       post: entity,
+      prev: prev ? toPostEntity(prev) : undefined,
+      next: next ? toPostEntity(next) : undefined,
       hideDate: (settings.postType?.hideDate ?? []).includes(entity.getPostType().value),
     },
   };
